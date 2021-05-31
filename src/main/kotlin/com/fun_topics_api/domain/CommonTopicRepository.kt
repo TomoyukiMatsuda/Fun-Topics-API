df3e8be5ee87cc0278289eeca929470fed873c10
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-06-01 07:41:41 +0900

    共通トピック削除APIを実装

--- src/main/kotlin/com/fun_topics_api/domain/CommonTopicRepository.kt
+++ src/main/kotlin/com/fun_topics_api/domain/CommonTopicRepository.kt
@@ -5,5 +5,9 @@ import com.fun_topics_api.domain.model.CommonTopic
 interface CommonTopicRepository {
     fun findAll(): List<CommonTopic>
 
+    fun find(id: Int): CommonTopic?
+
     fun create(commonTopic: CommonTopic)
+
+    fun delete(id: Int)
 }
